62ac6239da2fed69e98b7605c0ccd38253aa3259
inaralmeida <inara.ralmeida@gmail.com>
2025-06-27 00:15:23 -0300

    refactor: remove componente App e implementar rotas com styled-components

--- front-end/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>Todo App - Arquitetura Hexagonal</h1>
-    </div>
-  );
-}
-
-export default App;
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import { AppRoutes } from "./presentation/routes/Routes";
+import { GlobalStyle, theme } from "./presentation/style";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <AppRoutes />
+    </ThemeProvider>
   </React.StrictMode>
 );
